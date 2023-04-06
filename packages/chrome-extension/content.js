97b85cfb0243f84dd55ba326c02335b9ebdbdae8
Orkessler <okessler@g.jct.ac.il>
2023-04-07 02:13:18 +0300

    Injecting a response to Instagram, Facebook and Twitter.
    When the user clicks on the savee option in the context menu, the text he marked is injected into a comment on Facebook, Instagram, and Twitter.
    If the user is on another site, he will receive an alert

--- packages/chrome-extension/content.js
+++ packages/chrome-extension/content.js
@@ -1,12 +1,48 @@
-function createSelectedTextElem(text) {
-  const h = document.createElement("h2");
-  const node = document.createTextNode(text);
-  h.appendChild(node);
-  document.body.appendChild(h);
-}
+//test for creating a new element and appending it to the body
+// function createSelectedTextElem(text) {
+//   const div = document.createElement("div");
+//   const h = document.createElement("h2");
+//   const node = document.createTextNode(text);
+//   h.appendChild(node);
+//   div.appendChild(h);
+//   document.body.appendChild(div);
+// }
 
 chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
   if (message.text === "activate-savee") {
-    createSelectedTextElem(message.selection);
+    const currentUrl = window.location.href;
+    if (currentUrl.includes("twitter.com")) {
+      const textArea = document.querySelector('[data-testid="tweetTextarea_0"]');
+      if (textArea) {
+        textArea.focus();
+        document.execCommand("insertText", false, message.selection);
+      }
+    } else if (currentUrl.includes("facebook.com")) {
+      // Find the comment box for the post
+      const textArea = document.querySelector('[aria-label="כתיבת תגובה"]') || lastCommentBox.querySelector('[aria-label="Write a comment…"]');
+      if (textArea) {
+        textArea.focus();
+        document.execCommand("insertText", false, message.selection);
+
+      }
+    } else if (currentUrl.includes("instagram.com")) {
+      const textArea = document.querySelector('[aria-label="Add a comment…"]');
+      if (textArea) {
+        textArea.focus();
+        document.execCommand("insertText", false, message.selection);
+      }
+    } else {
+      alert("Savee can only be activated on Twitter, Facebook, or Instagram.");
+      //send alert in the user's language
+      // chrome.i18n.detectLanguage(function (result) {
+      //   console.log(result);
+      //   if (result.languages[0].indexOf("he") !== -1) {
+      //     alert("התוסף פועל רק בטוויטר, אינסטגרם ופייסבוק.");
+      //   } else {
+      //     alert("Savee can only be activated on Twitter, Facebook, or Instagram.");
+      //   }
+      // });
+    }
   }
 });
+

85ab3b6881a03ced3e292bae7f525930c0d3eb55
Orkessler <okessler@g.jct.ac.il>
2023-04-07 11:58:11 +0300

    Add getSaveeResponse function
    The function still needs to be connected to the server

--- packages/chrome-extension/content.js
+++ packages/chrome-extension/content.js
@@ -1,4 +1,5 @@
-//test for creating a new element and appending it to the body
+//test for creating a new element and appending it to the body for future use
+
 // function createSelectedTextElem(text) {
 //   const div = document.createElement("div");
 //   const h = document.createElement("h2");
@@ -8,32 +9,39 @@
 //   document.body.appendChild(div);
 // }
 
+function getSaveeResponse(post) {
+  //here we will get the response from the server
+  return "This post of yours, " + post + ", is not true. The Holocaust was and still is the most terrible world war.";
+}
+
 chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
   if (message.text === "activate-savee") {
+    const response = getSaveeResponse(message.selection);
     const currentUrl = window.location.href;
     if (currentUrl.includes("twitter.com")) {
       const textArea = document.querySelector('[data-testid="tweetTextarea_0"]');
       if (textArea) {
         textArea.focus();
-        document.execCommand("insertText", false, message.selection);
+        document.execCommand("insertText", false, response);
       }
     } else if (currentUrl.includes("facebook.com")) {
-      // Find the comment box for the post
       const textArea = document.querySelector('[aria-label="כתיבת תגובה"]') || lastCommentBox.querySelector('[aria-label="Write a comment…"]');
       if (textArea) {
         textArea.focus();
-        document.execCommand("insertText", false, message.selection);
+        document.execCommand("insertText", false, response);
 
       }
     } else if (currentUrl.includes("instagram.com")) {
       const textArea = document.querySelector('[aria-label="Add a comment…"]');
       if (textArea) {
         textArea.focus();
-        document.execCommand("insertText", false, message.selection);
+        document.execCommand("insertText", false, response);
       }
     } else {
       alert("Savee can only be activated on Twitter, Facebook, or Instagram.");
+
       //send alert in the user's language
+
       // chrome.i18n.detectLanguage(function (result) {
       //   console.log(result);
       //   if (result.languages[0].indexOf("he") !== -1) {

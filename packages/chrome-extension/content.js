536ca0515925783e84722cf7c2830f50c1244391
Orkessler <okessler@g.jct.ac.il>
2023-04-10 12:20:06 +0300

    Add savee response

--- packages/chrome-extension/content.js
+++ packages/chrome-extension/content.js
@@ -9,48 +9,76 @@
 //   document.body.appendChild(div);
 // }
 
-function getSaveeResponse(post) {
-  //here we will get the response from the server
-  return "This post of yours, " + post + ", is not true. The Holocaust was and still is the most terrible world war.";
+// function to call the Feedox API and return the response
+async function getSaveeResponse(post) {
+  const url = 'https://api.feedox.com/v1/ai/conversation/5579120785547-8400';
+  const config = {
+    headers: {
+      'accept': '*/*',
+      'content-type': 'application/json; charset=UTF-8',
+      'accept-language': 'en-GB,en;q=0.9,en-US;q=0.8,he;q=0.7,es;q=0.6'
+    }
+  };
+  const data = {
+    messages: [
+      {
+        role: 'user',
+        content: "input: \"" + post + "\""
+      }
+    ],
+    "userId": "HWYnPaVfS3aoTLVlVxmAD4ISgwi2"
+  };
+  try {
+    const response = await fetch(url, {
+      method: 'POST',
+      ...config,
+      body: JSON.stringify(data),
+    });
+    console.log(data);
+    const result = await response.json();
+    console.log(result);
+    console.log("your post is:" + post);
+    console.log("your response is:" + result[0].content);
+    if (result[0].content) {
+      return result[0].content;
+    }
+  } catch (error) {
+    console.log('error');
+    console.error(error);
+  }
 }
 
+// listener to receive message from background.js and execute getSaveeResponse function
 chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
   if (message.text === "activate-savee") {
-    const response = getSaveeResponse(message.selection);
     const currentUrl = window.location.href;
     if (currentUrl.includes("twitter.com")) {
       const textArea = document.querySelector('[data-testid="tweetTextarea_0"]');
       if (textArea) {
         textArea.focus();
-        document.execCommand("insertText", false, response);
+        getSaveeResponse(message.selection).then(response => {
+          console.log(response);
+          document.execCommand("insertText", false, response);
+        });
       }
     } else if (currentUrl.includes("facebook.com")) {
       const textArea = document.querySelector('[aria-label="כתיבת תגובה"]') || lastCommentBox.querySelector('[aria-label="Write a comment…"]');
       if (textArea) {
         textArea.focus();
-        document.execCommand("insertText", false, response);
-
+        getSaveeResponse(message.selection).then(response => {
+          document.execCommand("insertText", false, response);
+        });
       }
     } else if (currentUrl.includes("instagram.com")) {
       const textArea = document.querySelector('[aria-label="Add a comment…"]');
       if (textArea) {
         textArea.focus();
-        document.execCommand("insertText", false, response);
+        getSaveeResponse(message.selection).then(response => {
+          document.execCommand("insertText", false, response);
+        });
       }
     } else {
       alert("Savee can only be activated on Twitter, Facebook, or Instagram.");
-
-      //send alert in the user's language
-
-      // chrome.i18n.detectLanguage(function (result) {
-      //   console.log(result);
-      //   if (result.languages[0].indexOf("he") !== -1) {
-      //     alert("התוסף פועל רק בטוויטר, אינסטגרם ופייסבוק.");
-      //   } else {
-      //     alert("Savee can only be activated on Twitter, Facebook, or Instagram.");
-      //   }
-      // });
     }
   }
 });
-

1a661bc7ae60a9e1b8032a7507ddef504bd93740
Elya Livshitz <elya.livshitz@gmail.com>
2023-04-17 15:22:46 +0300

    + workarea for editing the input and generating the output manually on the webapp + changing behavior of extension to import the text into workarea + include savee-userid for future monitoring

--- packages/web/src/scripts/ts/app.helpers.ts
+++ packages/web/src/scripts/ts/app.helpers.ts
@@ -2,7 +2,14 @@ import { Vue, Deferred, libx, Toast } from '/frame/scripts/ts/browserified/frame
 import { Helpers as FrameHelpers } from 'frame.libx.js/build-web/scripts/ts/app/app.helpers.js';
 
 export class Helpers extends FrameHelpers {
-	
+	public static scrollTo(id: string) {
+		const element = document.getElementById(id);
+		if (element) {
+			element.scrollIntoView({ behavior: 'smooth', block: 'start' });
+		} else {
+			console.error('Element not found:', id);
+		}
+	}
 }
 
 export default Helpers;
